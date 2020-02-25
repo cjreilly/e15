71622dbc8a8f55ad83559e09b36213b7d0887afd
Susan Buck <susan@susanbuck.net>
2020-02-25 18:14:38 -0500

    Week 5 progress

--- bookmark/app/Http/Controllers/BookController.php
+++ bookmark/app/Http/Controllers/BookController.php
@@ -6,13 +6,36 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    /**
+     * GET /books
+     */
     public function index()
     {
         return 'Here are all the books...';
     }
 
+    /**
+     * GET /book/{title}
+     */
     public function show($title)
     {
-        return 'Results for the book: '.$title;
+        # Query the database for a book where the title = $title
+        # Return a view to show the book
+        # Include the book data
+        return 'Here are the details for the book: '.$title;
+    }
+
+    /**
+     * GET /filter/{$category}/{subcategory?}
+     */
+    public function filter($category, $subcategory = null)
+    {
+        $output = 'Here are all the books under the category '.$category;
+
+        if ($subcategory) {
+            $output .= ' and also the subcategory '.$subcategory;
+        }
+
+        return $output;
     }
 }
--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'PageController@welcome');
 
-Route::get('/', function () {
-    return '<h1>Bookmark</h1>';
-    //return view('welcome');
-});
+Route::get('/books', 'BookController@index');
+Route::get('/books/{title?}', 'BookController@show');
+Route::get('/filter/{category}/{subcategory?}', 'BookController@filter');
