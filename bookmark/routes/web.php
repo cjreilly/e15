72fbe5761d25b4df99ee01b049850711cd72a4c8
Susan Buck <susan@susanbuck.net>
2020-03-30 12:48:48 -0400

    Bookmark after Week 8 changes

--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -4,19 +4,47 @@ Route::get('/example2', function () {
     dump(Arr::sort(['a', 'z', 'g']));
 });
 
+Route::get('/debug', function () {
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
+
+
 # Example route used to demonstrate error pages
 Route::get('/example', function () {
-    $foo = [1,2,3];
+    //dump(config('mail.supportEmail'));
 
-    # dump, die
-    //dd($foo);
+    // $foo = [1,2,3];
 
-    # dump, die, debug
-    //ddd($foo);
+    // # dump, die
+    // //dd($foo);
 
-    Log::info($foo);
+    // # dump, die, debug
+    // //ddd($foo);
 
-    ddd(storage_path('temp'));
+    // Log::info($foo);
+
+    // ddd(storage_path('temp'));
 
     return view('abc');
 });
